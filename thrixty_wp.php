885a607757d6fe49f6687e1019f4d7cd4eba923c
Florian Heitmann <florianheitmann@Entwicklung-II.local>
2016-05-23 11:09:46 +0200

    Update Thrixty V2.2

--- thrixty_wp.php
+++ thrixty_wp.php
@@ -1,18 +1,18 @@
 <?php
 	/**
-	 * Plugin Name: Thrixty Player 2.1
+	 * Plugin Name: Thrixty Player 2.2
 	 * Plugin URI:
 	 * Description: Wordpress Plugin, that is building a Player for 360° photography.
 	 *   It uses Shortcodes to generate HTML-Code, ready to be used as the Players base.
 	 *   The versionnumber of this plugin reflects the version of the used ThrixtyPlayer.
 	 * Author: F.Heitmann @ Fuchs EDV
 	 * Author URI:
-	 * Version: 2.1
+	 * Version: 2.2
 	 *
 	 * @package Wordpress
 	 * @subpackage Thrixty Player
 	 * @since 4.1.0
-	 * @version 2.1
+	 * @version 2.2
 	 */
 
 
